e6efd835488b8c9ae2bea7aae31c5383d7a380ce
geidtiphong <geidtiphong@gmail.com>
2017-12-27 19:37:31 +0700

    comment body isActive methdo

--- src/pages/main-side-menu/main-side-menu.ts
+++ src/pages/main-side-menu/main-side-menu.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams, Nav } from 'ionic-angular';
 import { PageInterface } from '../../shared/interface/pages.interface';
 import { ThreeMenu } from '../../shared/page-menu/page-menu';
@@ -14,7 +14,7 @@ export class MainSideMenuPage {
 
   // Basic root for our content view
   rootPage: string = 'TabsPage';
-  
+
   // ThreeMenu from page-menu
   mymenu: PageInterface[] = ThreeMenu;
 
@@ -36,26 +36,28 @@ export class MainSideMenuPage {
     } else {
       // Tabs are not active, so reset the root page 
       // In this case: moving to or from SpecialPage
+      if (page.pageName == 'LoginPage') {
+        this.navCtrl.push('LoginPage');
+      }
       this.nav.setRoot(page.pageName, params);
     }
   }
 
   isActive(page: PageInterface) {
     // Again the Tabs Navigation
-    let childNav = this.nav.getActiveChildNav();
+    // let childNav = this.nav.getActiveChildNavs();
+    // if (typeof childNav !='undefined' && typeof childNav[0] !='undefined') {
+    //   if (childNav[0].getSelected() && childNav[0].getSelected().root === page.tabComponent) {
+    //     return 'primary';
+    //   }
+    //   return;
+    // }
 
-    if (childNav) {
-      if (childNav.getSelected() && childNav.getSelected().root === page.tabComponent) {
-        return 'primary';
-      }
-      return;
-    }
-
-    // Fallback needed when there is no active childnav (tabs not active)
-    if (this.nav.getActive() && this.nav.getActive().name === page.pageName) {
-      return 'primary';
-    }
-    return;
+    // // Fallback needed when there is no active childnav (tabs not active)
+    // if (this.nav.getActive() && this.nav.getActive().name === page.pageName) {
+    //   return 'primary';
+    // }
+    // return;
   }
 
 }
